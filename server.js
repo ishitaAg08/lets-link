df9e9c252d63bc10b1e6b197fbaa5c8ed67ef598
ishitaAg08 <64578556+ishitaAg08@users.noreply.github.com>
2021-07-09 16:35:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ const http = require("http")
 const app = express()
 const server = http.createServer(app)
 const path = require("path")
-const io = require("socket.io")(server, {
-	cors: {
-		origin: "http://localhost:3000",
-		methods: [ "GET", "POST" ]
-	}
-})
+const io = require("socket.io")(server)
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id)

abf4798c4b2caf163fcd7ff6cb9c5e488907a58c
ishitaAg08 <64578556+ishitaAg08@users.noreply.github.com>
2021-07-09 18:52:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ io.on("connection", (socket) => {
 	})
 })
 if(process.env.PROD){
-	app.use(express.static(path.join(__dirname,"./client/build")));
+	app.use(express.static(path.join(__dirname,"./client/public")));
 	app.get('*',(req,res)=>{
-		res.sendFile(path.join(__dirname,'./client/build/index.html'))
+		res.sendFile(path.join(__dirname,'./client/public/index.html'))
 	})
 }
 

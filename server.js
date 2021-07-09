b690af392068ce989bc9249325cde0c0e47cb67d
ishitaAg08 <64578556+ishitaAg08@users.noreply.github.com>
2021-07-09 20:41:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ io.on("connection", (socket) => {
 		io.to(data.to).emit("callAccepted", data.signal)
 	})
 })
-if(process.env.PROD){
+if(process.env.NODE_ENV == "production"){
 	app.use(express.static(path.join(__dirname,"client/public")));
 	/*app.get('*',(req,res)=>{
 		res.sendFile(path.join(__dirname,'./client/public/index.html'))
